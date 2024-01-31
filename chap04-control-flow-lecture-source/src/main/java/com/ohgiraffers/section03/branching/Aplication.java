99d15e4ddc684a4f5854bc3ee1a5ffd86e8ded61
NoodleSh <na619620@naver.com>
2024-01-31 18:22:00 +0900

    240131 end commit

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Aplication.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section03/branching/Aplication.java
@@ -4,8 +4,10 @@ public class Aplication {
 
     public static void main(String[] args) {
         A_break a = new A_break();
+        B_continue b = new B_continue();
        // a.simpleBreakStaement();
         // a.simpleBreakStatement2();
-        a.testJumpBreak();
+        //a.testJumpBreak();
+        b.simpleContinueStatement2();
     }
 }
