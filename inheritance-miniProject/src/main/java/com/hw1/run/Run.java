2cb09e6d2eb968a32d624512905b0827b06df229
NoodleSh <na619620@naver.com>
2024-02-08 17:36:56 +0900

    240208 inheritance(상속 프로젝트 Run 주석 수정)

--- inheritance-miniProject/src/main/java/com/hw1/run/Run.java
+++ inheritance-miniProject/src/main/java/com/hw1/run/Run.java
@@ -14,7 +14,7 @@ public class Run {
 
         
         
-        Person[] studentArray = new Student[3];
+        Student[] studentArray = new Student[3];
 
         studentArray[0] = new Student(1,"정보시스템공학과", "홍길동",20,178.2,70.0);
         studentArray[1] = new Student(2,"경영학과","김말똥",21,187.3,80.0);
@@ -77,7 +77,6 @@ public class Run {
 
             if(chk == 'Y' || chk == 'y'){
                 ++count;
-
             }else {
                 System.out.println("입력을 중단합니다.");
                 break;
@@ -85,20 +84,40 @@ public class Run {
 
         }
 
+
+        /*
+         * employeeArray.length로 조회 할 경우, 값이 들어가지 않은
+         * 배열의 경우 null값이므로 nullPointerException 발생,
+         * 그러므로, count값으로 조회할 경우, count값만큼 배열이 들어가있기 때문에
+         * 생성된 배열의 길이만 조회하므로 오류가 발생하지 않는다.
+         * */
+
+        
         /*for(Employee emp : employeeArray){
             if(emp != null)
             System.out.println(emp.getInformation());
-        }*/ // for-each
+        }*/ // for-each(employeeArray로 조회할 경우)
 
-        
-        
-       /* for(int i= 0; i < employeeArray.length; i++){
+        /*for(Employee emp : count){
+            System.out.println(emp.getInformation());
+        }*/ // for-each(count로 조회할 경우)
+
+
+
+      /*  for(int i= 0; i < employeeArray.length; i++){
             if(employeeArray[i] != null) {
                 System.out.println(employeeArray[i].getInformation());
             }else{
                 break;
             }
-        }*/ // 일반 for문
+        }
+        */ // 일반 for문(employeeArray.length로 조회할 경우)
+
+
+        for(int i= 0; i < count; i++){
+            System.out.println(employeeArray[i].getInformation());
+            }
+        } // 일반 for문(count로 조회할 경우)
 
     }
 }
