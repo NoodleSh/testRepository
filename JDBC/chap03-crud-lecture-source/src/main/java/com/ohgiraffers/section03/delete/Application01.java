3c8cf1a37f7373fae5c29bf147675c28e655841e
NoodleSh <na619620@naver.com>
2024-02-29 15:59:22 +0900

    delete commit

---
+++ JDBC/chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section03/delete/Application01.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section03.delete;
+
+import java.util.Scanner;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+        /* 1. 삭제할 메뉴 코드 입력받기 */
+
+        /* 2. DeleteController의 deleteMenu() 메소드 호출 */
+
+        /* 3. delete 결과에 따라 성공이면 '메뉴 삭제 성공!' 실패이면 '메뉴 삭제 실패 ㅜㅜ' 출력 */
+
+        /* entry key = "deleteMenu" */
+
+        Scanner sc  = new Scanner(System.in);
+
+        System.out.println("삭제할 메뉴 코드 : ");
+        int menuCode = sc.nextInt();
+
+
+
+    }
+}
